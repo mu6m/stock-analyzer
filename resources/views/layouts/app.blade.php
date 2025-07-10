9f48cf18330658ab1721e8f9b5d11e9081ea81a3
ledraa <theledraa@gmail.com>
2025-07-10 12:06:53 +0300

    fixing vercel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'أسهم')</title>
+    
     @vite('resources/css/app.css')
+    
     <script>
         tailwind.config = {
             theme: {
